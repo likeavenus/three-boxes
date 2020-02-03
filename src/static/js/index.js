72b5c69144e3fffce1d50f6baf9526e85f9f3bc2
Rafael <rafael_guliev94@list.ru>
2020-02-04 00:22:55 +0300

    master | initial commit

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,1 +1,32 @@
+import * as THREE from 'three';
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
+(()=> {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    const renderer = new THREE.WebGLRenderer();
+    const pageWrapper = document.querySelector('.page__wrapper');
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    pageWrapper.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial();
+    const cube = new THREE.Mesh(geometry);
+
+    for (let i = 0; i < geometry.faces.length; i++) {
+        geometry.faces[i].color.setHex(0xff0000 )
+    }
+    console.log(geometry);
+    console.log(cube);
+    scene.add(cube);
+
+    camera.position.z = 10;
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    }
+    animate();
+})();
