2e4854f78c895731a7e8fa8d5cbb6b2d436de46e
Rafael <rafael_guliev94@list.ru>
2020-02-04 17:07:39 +0300

    master | делает сетку из кубов

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -11,22 +11,68 @@ import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
     pageWrapper.appendChild(renderer.domElement);
 
     const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial();
-    const cube = new THREE.Mesh(geometry);
+    const material = new THREE.MeshBasicMaterial({color: 0x00ff00, vertexColors: THREE.FaceColors});
+    const colorHSL = {};
 
-    for (let i = 0; i < geometry.faces.length; i++) {
-        geometry.faces[i].color.setHex(0xff0000 )
+    const cubesArr = [
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+        new THREE.Mesh(geometry, material),
+    ];
+
+    let marginX = 0;
+    let marginY = 0;
+    for (let i = 0; i < cubesArr.length; i++) {
+        if (i !== 0 && i % 5 === 0) {
+            marginX = 0;
+            marginY += 1;
+        }
+        cubesArr[i].position.x += marginX * 2;
+        cubesArr[i].position.y = marginY * 2;
+
+        scene.add(cubesArr[i]);
+        marginX += 1;
     }
-    console.log(geometry);
-    console.log(cube);
-    scene.add(cube);
 
-    camera.position.z = 10;
+    console.log(cubesArr);
+
+    material.color.getHSL( colorHSL );
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.x = -10;
+
+
+    camera.position.z = 21;
+    scene.children[0].geometry.faces[0].color.setHSL(1, colorHSL.s, colorHSL.l);
+    scene.children[0].geometry.faces[1].color.setHSL(colorHSL.h, colorHSL.s, colorHSL.l);
 
     const controls = new OrbitControls(camera, renderer.domElement);
     function animate() {
         requestAnimationFrame(animate);
         renderer.render(scene, camera);
+
+        // geometry.elementsNeedUpdate = true;
     }
     animate();
 })();
